358cd810c328e0f2e83a8b5db31fb687dbb1e3d7
Mingbao <mingbaozhu2012@gmail.com>
2015-02-01 16:52:45 +0800

    Readme

---
+++ IWatchLoading/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  IWatchLoading
+//
+//  Created by Mingbao on 15/1/25.
+//  Copyright (c) 2015年 Mingbao. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Readme
@@ -0,0 +1,5 @@
+# LoadingView
+### 简单模仿一下iWatch Loading View
+### 用Swift实现
+
+![效果](./loadingview.gif)
